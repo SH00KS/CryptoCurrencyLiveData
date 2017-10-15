39fb9eb52161962071baa4d55d97447938399182
SH00KS <carvill@programmer.net>
2017-10-15 20:30:03 +0100

    Auto update the currency. 🌀
    Updating the crypto values automatically without having to refresh the
    page. ⏲

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 var cryptoCurrencyLiveData = angular.module('cryptoCurrencyLiveData',[]);
 
-cryptoCurrencyLiveData.controller('MainController', ['$scope', '$http', function($scope, $http){
+cryptoCurrencyLiveData.controller('MainController', ['$scope', '$http', '$interval', function($scope, $http, $interval){
   $scope.appName = "CrpytoCurrency Values";
-  $http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=XRP&tsyms=USD,BTC,ETH")
-   .then(function(response) {
-       var cryptoData = JSON.stringify(response.data);
-       $scope.cryptoData = JSON.parse(cryptoData);
-   });
+
+  $interval(function() {
+    $http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=XRP&tsyms=USD,BTC,ETH")
+     .then(function(response) {
+         var cryptoData = JSON.stringify(response.data);
+         $scope.cryptoData = JSON.parse(cryptoData);
+     });         
+      }, 1000);
+
 }]);
