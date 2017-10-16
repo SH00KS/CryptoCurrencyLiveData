cbc0a85793343b59143fcd2893211a71f89d296c
SH00KS <carvill@programmer.net>
2017-10-16 23:15:15 +0100

    Configure data-ng-bind's and ng-clicks 👈🏽
    ..to update the data in the appropriate fields on the screen

--- js/index.js
+++ js/index.js
@@ -4,11 +4,19 @@ cryptoCurrencyLiveData.controller('MainController', ['$scope', '$http', '$interv
   $scope.appName = "CrpytoCurrency Values";
 
   $interval(function() {
-    $http.get("https://api.coinmarketcap.com/v1/ticker/")
+    $http.get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
      .then(function(response) {
          var cryptoData = JSON.stringify(response.data);
          $scope.cryptoData = JSON.parse(cryptoData);
+         $scope.cryptoName = $scope.cryptoData.name;
+
+         $scope.changeView = function(ticker) {
+          console.log("changeView called with ", ticker, $scope.cryptoData);
+         var currency = $scope.cryptoData.find(x => x.symbol === ticker);
+         console.log("currency is ", currency);
+          $scope.currentSelectedCurrency = currency;
+
+}
      });
    }, 10000);
-
 }]);
