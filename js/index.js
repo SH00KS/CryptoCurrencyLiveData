04dd9e12e003856030a1878df1dea3c69efd3ca4
SH00KS <carvill@programmer.net>
2017-10-18 21:47:10 +0100

    Fix of data switching to top coin issue, break out JS into functions 👌🏽
    Break out of api into variable, create function for populating the
    initial data. General tidy up. 🚿

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var cryptoCurrencyLiveData = angular.module('cryptoCurrencyLiveData',[]);
 
 cryptoCurrencyLiveData.controller('MainController', ['$scope', '$http', '$interval', function($scope, $http, $interval){
-  $scope.appName = "CrpytoCurrency Values";
+  var getCurrencyData = $http.get("https://api.coinmarketcap.com/v1/ticker/");
 
   $scope.changeView = function(ticker) {
     var currency = $scope.cryptoData.find(x => x.symbol === ticker);
@@ -9,17 +9,24 @@ cryptoCurrencyLiveData.controller('MainController', ['$scope', '$http', '$interv
   };
 
   function loadData(){
-    $http.get("https://api.coinmarketcap.com/v1/ticker/")
+    getCurrencyData
      .then(function(response) {
          $scope.cryptoData = response.data;
-         $scope.currentSelectedCurrency = response.data[0];
-         console.log($scope.topCoin);
        });
   };
+
+  function populateInitialData(){
+    getCurrencyData
+     .then(function(response) {
+       $scope.topCoin = response.data[0];
+       $scope.currentSelectedCurrency = $scope.topCoin;
+     });
+  }
 
   $interval(function() {
     loadData();
    }, 10000);
 
    loadData();
+   populateInitialData();
 }]);
